a6bb1eeb2f32689f3e91db82fd0d47cffd961528
luizSiewerdt <l.siewerdt@greylogix.com.br>
2025-03-09 05:03:17 -0300

    Add treesitter commentcontext

--- .config/nvim/lua/shampooshrek/plugins/treesitter.lua
+++ .config/nvim/lua/shampooshrek/plugins/treesitter.lua
@@ -102,5 +102,13 @@ return {
       end
     end
   end,
+},
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    config = function()
+      require('ts_context_commentstring').setup {
+  enable_autocmd = false,
 }
+    end
+  }
 }
